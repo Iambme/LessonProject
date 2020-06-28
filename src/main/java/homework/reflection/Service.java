489edf8d40b8a9873f80155e6465fdcdac7068ad
Pavel Lyamin <iambme2@gmail.com>
2020-06-28 13:27:02 +0300

    little changes

--- src/main/java/homework/reflection/Service.java
+++ src/main/java/homework/reflection/Service.java
@@ -1,36 +1,10 @@
 package homework.reflection;
-
-import homework.io.ex3.MyReader;
-import homework.io.ex3.MyReaderWriterService;
-import homework.io.ex3.MyWriter;
-
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Service {
-    /*
-Создать методы:
-создать заказ
-добавить блюдо в клиентский заказ
-посчитать стоимость клиентского заказа
-выдать клиенту чек (выводит на консоль список заказанных блюд, их стоимость и общую сумму)
-
-В мейне создать меню:
-1. Создать заказ (пользователь вводит свое имя)
-2. Добавить заказ клиенту (пользователь вводит свое имя и желаемое блюдо)
-3. Расчитать клиента (выводится чек и заказ удаляется из системы)
-4. Вывести всех нерасчитанных клиентов (выводятся имя и номер клиентов, которые еще не рассчитались)
-
-
-В мейне все объекты классов создавать через reflection, методы этих объектов также вызывать через reflection.
-Создать JUnit тесты для методов из класса Service: создание заказа, добавление блюда в заказ и подсчет стоимости заказа.
-     */
     private Map<Client, Order> orderMap = new HashMap<>();
 
 
@@ -48,39 +22,30 @@ public class Service {
             System.out.println("5.Выход");
             int userInput = Integer.parseInt(reader.readLine());
             switch (userInput) {
-                case 1: {
+                case 1 -> {
                     System.out.println("Введите имя");
                     String name = reader.readLine();
                     createClient(name);
 
 
                 }
-                break;
-                case 2: {
+                case 2 -> {
                     System.out.println("Введите имя");
                     String name = reader.readLine();
                     createOrder(name);
                 }
-
-                break;
-                case 3: {
+                case 3 -> {
                     System.out.println("Введите имя");
                     String name = reader.readLine();
                     check(new Client(name));
                 }
-                break;
-
-                case 4: {
+                case 4 -> {
                     System.out.println("Ещё не расчитались");
                     for (Map.Entry<Client, Order> entry : orderMap.entrySet()) {
                         System.out.println(entry.getKey().getName() + " номер заказа " + entry.getKey().getNumber());//рефлексия
                     }
                 }
-                break;
-
-                case 5:
-                    isOver = true;
-                    break;
+                case 5 -> isOver = true;
             }
         }
 
@@ -110,14 +75,14 @@ public class Service {
     private Dish createDish(int arg) throws Exception {
         Class<Dish> dishClass = Dish.class;
         switch (arg) {
-            case 1: {
+            case 1 -> {
                 return (Dish) dishClass.getDeclaredConstructor(String.class, int.class).newInstance("Суп с фрикадельками", 50);
 
             }
-            case 2: {
+            case 2 -> {
                 return (Dish) dishClass.getDeclaredConstructor(String.class, int.class).newInstance("Котлетки с пюрешкой", 70);
             }
-            case 3: {
+            case 3 -> {
                 return (Dish) dishClass.getDeclaredConstructor(String.class, int.class).newInstance("Маффин", 20);
             }
         }
